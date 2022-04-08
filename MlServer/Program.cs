4aeeb79a10504d371bc9d8a143a581211e2130fb
Sergei (Mac) <sergeinosov6@gmail.com>
2022-04-08 23:19:18 +0300

    initial commit

---
+++ MlServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
